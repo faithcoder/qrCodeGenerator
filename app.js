3b4619af6e3eb5c41426b1b74e1497bf7edccb43
Abdullah Al Arif <140299847+arif-xs@users.noreply.github.com>
2024-01-07 23:11:34 +0600

    if condition added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQR() {
-  qrImage.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrText.value;
+  if (qrText.value > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+  }
 }
